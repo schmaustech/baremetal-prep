3ddd845e664b712ff380914d6adba0dd1e30979a
Benjamin Schmaus <bschmaus@redhat.com>
2019-11-24 11:12:41 -0600

    Update baremetal-prep.sh

--- baremetal-prep.sh
+++ baremetal-prep.sh
@@ -64,8 +64,8 @@ setup_repository(){
   sudo firewall-cmd --reload
   sudo podman create --name poc-registry -p 5000:5000 -v /opt/registry/data:/var/lib/registry:z -v /opt/registry/auth:/auth:z -e "REGISTRY_AUTH=htpasswd" -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry" -e "REGISTRY_HTTP_SECRET=ALongRandomSecretForRegistry" -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd -v /opt/registry/certs:/certs:z -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key docker.io/library/registry:2
   sudo podman start poc-registry
-  mirror_images()
-  update_installconfig()
+  mirror_images
+  update_installconfig
 }
 
 update_installconfig(){
